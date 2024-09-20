d26865132ecf2c7b118e342c9b511114a924d0be
Seb <segomezru@gmail.com>
2024-09-20 11:55:51 +0000

    Docker changes

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 ### Domain Shift Segmentation Quality
 <b>Actual title:</b> Estimating image segmentation quality under domain shift via dimensionality reduction in feature space (in the context of MRI)
 
-#### 17.09.2024
+#### STILL TO BE UPDATED THIS README FOR INSTRUCTIONS OF EASY LAUNCH OFF
+
+#### 20.09.2024
+
+Being inside the cloned repository directory, switch directories to the "docker" directory, and run the following command in the terminal to build the docker image:
+```bash
+sh build.sh -t dssq
+```
 
 I will still update this README for giving instructions on the how to replicate, for now:
 
@@ -11,6 +18,11 @@ I will still update this README for giving instructions on the how to replicate,
 
 - To collect the data (perform all experiments): src/get_data.py or src/get_data_scanner.py
 
-#### DATA link
+#### Weights and experiment collected data
 
 [Drive with model weights and collected data](https://drive.google.com/drive/folders/1zDsbSquuFGKFuvTujU_OwrbsECdbWGHz?usp=sharing)
+
+#### Datasets
+
+- [MSPMRI](https://liuquande.github.io/SAML/)
+- [M\&Ms-2](https://www.ub.edu/mnms-2/)
--- docker/run.sh
+++ docker/run.sh
@@ -9,17 +9,21 @@ This script builds the Docker image for the project.
 Options:
   -h, --help      Display this help message and exit
   -t <name>       Process the name provided with the -t option
+  -d <path>       Specify the data input path
+  -o <path>       Specify the output path
 
 Example:
-  $0 -t projectA  Builds an image with name projectA
+  $0 -t projectA -d /path/to/data -o /path/to/output
 
 EOF
 )
 
 # Initialize our variables
 name=""
+datapath=""
+outpath=""
 
-while getopts ":ht:" opt; do
+while getopts "hd:o:t:" opt; do
   case ${opt} in
     h )
       echo "$helpMessage"
@@ -27,6 +31,12 @@ while getopts ":ht:" opt; do
       ;;
     t )
       name="$OPTARG"
+      ;;
+    d )
+      datapath="$OPTARG"
+      ;;
+    o )
+      outpath="$OPTARG"
       ;;
     \? )
       echo "Invalid Option: -$OPTARG" 1>&2
@@ -39,21 +49,29 @@ while getopts ":ht:" opt; do
   esac
 done
 
-# Check if the name is provided
+# Check if the required options are provided
 if [ -z "$name" ]; then
-    echo "No name provided. Use -t option."
-else
-    echo "Processing name: $name"
+    echo "Error: No name provided. Use -t option."
+    exit 1
+fi
+if [ -z "$datapath" ]; then
+    echo "Error: No data input path provided. Use -d option."
+    exit 1
+fi
+if [ -z "$outpath" ]; then
+    echo "Error: No data output path provided. Use -o option."
+    exit 1
 fi
 
+# Docker run command
 docker run \
-	-it \
-	--net=host \
-	--runtime=nvidia \
+  -it \
+  --net=host \
+  --runtime=nvidia \
   --gpus all \
   --cpus=14 \
   --privileged \
-	--ipc=host \
-	--mount type=bind,source="/home/gomez/out",target=/workspace/out \
-	--mount type=bind,source="/home/gomez/Data/",target=/data/Data \
-	$name
+  --ipc=host \
+  --mount type=bind,source="$outpath",target=/workspace/out \
+  --mount type=bind,source="$datapath",target=/data/Data \
+  "$name"
